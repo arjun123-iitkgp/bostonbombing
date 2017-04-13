049b2ac61dcc4f8be0f65da7226e93e8cc22806d
mraduldubey <mraduldubeymd19@gmail.com>
2017-04-13 15:06:15 +0530

    csv generated

--- twitter-files/analysis.py
+++ twitter-files/analysis.py
@@ -7,7 +7,10 @@ jsonfile = twitter_samples.fileids()[-1]
 #absolute path for the file: #input_file = os.path.abspath(jsonfile)=>returns virtualenv file path
 input_file = twitter_samples.abspath(jsonfile) #returns system /usr/share/ path
 
-with open(input_file) as fp:
-	json2csv(fp,'tweets_text.csv',['text']) #json2csv(pointer, nameoffile, [feature1,feature2,feature3])
+#with open(input_file) as fp:
+	#json2csv(fp,'tweets_text.csv',['text']) #json2csv(pointer, nameoffile, [feature1,feature2,feature3])
 
 #think about the attributes to be imported, convert to panda, make a dataframe, apply stemming to tweet texts, save them. 
+with open(input_file) as fp:
+	json2csv(fp, 'tweets_dataframe.csv',['id','text','user.favourites_count','user.id','lang','user.followers_count','user.verified','truncated'])
+
